042007aa188cc2f621ef4ecc63e17ec0d60bb7e6
Capitão TI <gustavoflautist@gmail.com>
2025-08-25 11:15:11 -0300

    Att

--- main.js
+++ main.js
@@ -3,16 +3,22 @@
 const express = require('express');
 const simuladorRoutes = require('./api/routes/simulador_routes.js');
 const authenticateToken = require('./api/middleware/auth.js');
+const swaggerUi = require('swagger-ui-express');
+const YAML = require('yamljs');
+const swaggerDocument = YAML.load('./openapi.yaml');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 
+// 1. Rota da documentação (pública)
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
+// 2. Middleware de autenticação (só se aplica ao que vem depois)
 app.use(authenticateToken);
 
-// 3. SUAS ROTAS CONTINUAM AQUI
+// 3. Rotas da API (protegidas)
 app.use('/api', simuladorRoutes);
 
 app.get('/', (req, res) => {
@@ -21,4 +27,5 @@ app.get('/', (req, res) => {
 
 app.listen(PORT, () => {
     console.log(`Servidor rodando na porta http://localhost:${PORT}`);
+    console.log(`Documentação disponível em http://localhost:${PORT}/api-docs`); // Linha extra para facilitar :)
 });
